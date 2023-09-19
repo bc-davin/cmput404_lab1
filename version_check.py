fd851245ec51ff7c9c5fa1a3b7b84b53179dce07
Davin <71451687+bc-davin@users.noreply.github.com>
2023-09-18 18:10:31 -0600

    Add files via upload

--- version_check.py
+++ version_check.py
@@ -1,14 +1,16 @@
 import requests
 
 def main():
-    url = "https://www.google.com"
+    url = "https://raw.githubusercontent.com/bc-davin/cmput404_lab1/main/version_check.py"
 
     try:
         response = requests.get(url)
         response.raise_for_status() 
-
-        print(f"Status Code: {response.status_code}")
-        print(f"Google Homepage Content:\n{response.text[:1000]}")  
+        if response.status_code==200:
+            source_code=response.text
+            print(source_code)
+        #print(f"Status Code: {response.status_code}")
+        #print(f"Google Homepage Content:\n{response.text[:1000]}")  
 
     except requests.exceptions.RequestException as e:
         print(f"Error: {e}")
